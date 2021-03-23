98a050cb485be81accb6b604b1699dd50800281e
Iván Serrano <contacto.ivanserrano@gmail.com>
2021-03-23 16:45:55 -0600

    Se envian cambios

--- Python/account.py
+++ Python/account.py
@@ -1,6 +1,10 @@
-class Accounr:
+class Account:
     id          = int
     name        = str
     document    = str 
     emai        = str
     password    = str
+
+    def __init__(self, name, document):
+        self.name = name
+        self.document = document
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,22 @@
+from car import Car
+from account import Account
+
 if __name__ == "__main__":
-    print("Hola, Mundo")
+    print("Hola Mundo")
+
+    car = Car("AMS234", Account("Ivan Serrano", "SECI900801Q31"))
+    print(vars(car))
+    print(vars(car.driver))
+    
+# if __name__ == "__main__":
+#     print("Hola Mundo")
+#     car = Car()
+#     car.license = "AMS234"
+#     car.driver = "Ivan Serrano"
+#     print(vars(car))
+
+#     car2 = Car()
+#     car2.license = "QWE567"
+#     car2.driver = "Kiara Gomez"
+#     print(vars(car2))
+
